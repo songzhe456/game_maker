15ac291591057cd2e876eeb399f49edcdf4161dd
Zhe Song <3445189607@qq.com>
2026-09-16 16:03:29 +0800

    现在构建脚本会在完成时输出

--- build.py
+++ build.py
@@ -1,3 +1,9 @@
 import subprocess
-
-subprocess.Popen(["cargo","build","--release"]).communicate()
+def run():
+    build = subprocess.run(["cargo","build","--release"],capture_output=True)
+    if build.returncode == 0:
+        print(f"构建成功")
+    else:
+        print(f"构建失败:{str(build.stderr)}")
+if __name__ == "__main__":
+    run()
