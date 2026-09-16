b89246047344b532d9e0f5dd1767a9caec61f6e7
Zhe Song <3445189607@qq.com>
2026-09-16 16:48:02 +0800

    优化代码结构

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,28 @@ const END_MSG : &str = "程序已结束";
 fn main() {
     println!("欢迎使用{:?}",NAME);
     println!("请输入”继续“");
-    //读取用户输入，如果读到"继续"，会继续流程（当前无后续流程）
+    //读取用户输入，如果读到"继续"，会继续流程
+    get_input();
+    end_program(exit_codes::ExitCodes::SUCCESS as i32);
+}
+
+fn create_game_dir(){
+    let game_dir = fs::create_dir("game");
+    match game_dir {
+        Ok(_d) => println!("文件夹创建成功!"),
+        Err(e) if e.kind() != ErrorKind::AlreadyExists=> println!("{:?}文件夹创建失败!",e),
+        Err(_a) => println!("文件已存在"),
+    }
+}
+
+fn get_input(){
     let mut input = String::new();
     std::io::stdin()
     .read_line(&mut input)
     .expect("读取失败");
     //读到继续后会创建新文件夹用于存放相关文件
     if input.trim() == "继续"{
-        let game_dir = fs::create_dir("game");
-        match game_dir {
-            Ok(_d) => println!("文件夹创建成功!"),
-            Err(e) if e.kind() != ErrorKind::AlreadyExists=> println!("{:?}文件夹创建失败!",e),
-            Err(_a) => println!("文件已存在"),
-        }
-        end_program(exit_codes::ExitCodes::SUCCESS as i32);
+        create_game_dir();
     }
 }
 
